ba11c7cb8d0b2add950634b979c89ad1c636f49d
UraR1 <ura.r1@mail.ru>
2024-05-23 21:22:48 +0500

    22.05

--- src/backend/login.py
+++ src/backend/login.py
@@ -2,15 +2,15 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import StringProperty
 from kivy.app import App
 from database.database import validate_login
-from kivy.utils import platform
+#from kivy.utils import platform
 
 class LoginScreen(BoxLayout):
     error_message = StringProperty("")
 
     def login(self, username, password):
-        if platform == "android":
-            from android.permissions import request_permissions, Permission
-            request_permissions([Permission.READ_EXTERNAL_STORAGE,Permission.CAMERA, Permission.WRITE_EXTERNAL_STORAGE])
+        #if platform == "android":
+        #    from android.permissions import request_permissions, Permission
+        #    request_permissions([Permission.READ_EXTERNAL_STORAGE,Permission.CAMERA, Permission.WRITE_EXTERNAL_STORAGE])
         if validate_login(username, password):
             self.error_message = ""
             app = App.get_running_app()
