74d1cf5b3cfe4fd7da5cdcaa07aa44a069769854
UraR1 <ura.r1@mail.ru>
2024-06-04 01:44:35 +0500

    04.06

--- src/backend/RecordApp.py
+++ src/backend/RecordApp.py
@@ -28,7 +28,7 @@ class RecordAppScreen(Screen):
             number = get_current_number()
         os_name = platform.system()
         self.info_message = os_name
-        if os_name == 'Android':
+        if os_name == 'Linux':
             # Запрос разрешений для Android
             from android.permissions import request_permissions, Permission
             request_permissions([Permission.CAMERA, Permission.WRITE_EXTERNAL_STORAGE])
