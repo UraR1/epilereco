20779bb827e110784aedf8fa3dc1adef41964ab7
UraR1 <ura.r1@mail.ru>
2024-06-04 16:44:21 +0500

    04.06

--- src/backend/RecordApp.py
+++ src/backend/RecordApp.py
@@ -32,29 +32,53 @@ class RecordAppScreen(Screen):
             # Запрос разрешений для Android
             from android.permissions import request_permissions, Permission
             request_permissions([Permission.CAMERA, Permission.WRITE_EXTERNAL_STORAGE, Permission.INTERNET, Permission.READ_EXTERNAL_STORAGE])
-            self.capture = cv2.VideoCapture(0)
+            cap = cv2.VideoCapture(0)
+
+            # Определяем кодек и создаем объект VideoWriter
+            fourcc = cv2.VideoWriter_fourcc(*'XVID')
+            out = cv2.VideoWriter('output.avi', fourcc, 20.0, (640, 480))
+
+            while (cap.isOpened()):
+                # Захватываем кадр за кадром
+                ret, frame = cap.read()
+                if ret == True:
+                    # Записываем кадр в файл
+                    out.write(frame)
+
+                    # Отображаем кадр
+                    cv2.imshow('frame', frame)
+                    if cv2.waitKey(1) & 0xFF == ord('q'):
+                        break
+                else:
+                    break
+
+            # Освобождаем все ресурсы
+            cap.release()
+            out.release()
+            cv2.destroyAllWindows()
         else:
             self.capture = cv2.VideoCapture(0)
+            self.fourcc = cv2.VideoWriter_fourcc(*'XVID')
+            now = datetime.datetime.now()
+            date_string = now.strftime("%Y-%m-%d_%H-%M-%S")  # Формат: ГГГГ-ММ-ДД_ЧЧ-ММ-СС
+            user_data_dir = App.get_running_app().user_data_dir
+            # Создаем путь к папке с номером пользователя
+            user_folder_path = os.path.join(user_data_dir, str(number))
+            if not os.path.exists(user_folder_path):
+                os.makedirs(user_folder_path)
+            filename = os.path.join(user_folder_path, f"{number}_{date_string}.avi")
+            # print(f'Папка "{filename}" успешно создана!')
+            self.out = cv2.VideoWriter(filename, self.fourcc, 20.0, (640, 480))
+            Clock.schedule_once(self.update, 1 / 30.)
+
+            with connect() as connection:
+                cursor = connection.cursor()
+                number = get_current_number()
+                cursor.execute("INSERT INTO video (number,video_name) VALUES (?, ?)", (number, date_string))
+                connection.commit()
 
 
-        self.fourcc = cv2.VideoWriter_fourcc(*'XVID')
-        now = datetime.datetime.now()
-        date_string = now.strftime("%Y-%m-%d_%H-%M-%S")  # Формат: ГГГГ-ММ-ДД_ЧЧ-ММ-СС
-        user_data_dir = App.get_running_app().user_data_dir
-        # Создаем путь к папке с номером пользователя
-        user_folder_path = os.path.join(user_data_dir, str(number))
-        if not os.path.exists(user_folder_path):
-            os.makedirs(user_folder_path)
-        filename = os.path.join(user_folder_path, f"{number}_{date_string}.avi")
-        #print(f'Папка "{filename}" успешно создана!')
-        self.out = cv2.VideoWriter(filename, self.fourcc, 20.0, (640, 480))
-        Clock.schedule_once(self.update, 1 / 30.)
 
-        with connect() as connection:
-            cursor = connection.cursor()
-            number = get_current_number()
-            cursor.execute("INSERT INTO video (number,video_name) VALUES (?, ?)", (number,date_string))
-            connection.commit()
 
     def stop_recording(self, instance):
         self.recording = False
